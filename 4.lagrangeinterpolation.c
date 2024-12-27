e8e77a2f17ace419fad1d1cdf6691bbbb91f6917
ayush-tomer <ayushtomer012@gmail.com>
2024-12-27 13:13:01 +0530

    Update

---
+++ 4.lagrangeinterpolation.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    double x[100], y[100], xp, yp = 0, p;
+    printf("Enter number of data: ");
+    scanf("%d", &n);
+    printf("Enter data:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("x[%d] = ", i);
+        scanf("%lf", &x[i]);
+        printf("y[%d] = ", i);
+        scanf("%lf", &y[i]);
+    }
+    printf("Enter interpolation point: ");
+    scanf("%lf", &xp);
+    for (i = 0; i < n; i++)
+    {
+        p = 1;
+        for (j = 0; j < n; j++)
+        {
+            if (j != i)
+            {
+                p = p * (xp - x[j]) / (x[i] - x[j]);
+            }
+        }
+        yp += p * y[i];
+    }
+    printf("Interpolated value at %lf is %lf.", xp, yp);
+    return 0;
+}
---
+++ 5.newtondivideddifferenceinterpolation.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    double x[100], y[100], p[100][100], k, f, f1;
+    printf("Enter number of observations: ");
+    scanf("%d", &n);
+    printf("Enter corresponding values of x:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%lf", &x[i]);
+    }
+    printf("Enter corresponding values of y:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%lf", &y[i]);
+        p[i][0] = y[i];
+    }
+    printf("Enter the value of x for which you want to find y: ");
+    scanf("%lf", &k);
+    for(j = 1; j < n; j++)
+    {
+        for(i = 0; i < n - j; i++)
+        {
+            p[i][j] = (p[i + 1][j - 1] - p[i][j - 1]) / (x[i + j] - x[i]);
+        }
+    }
+    f = y[0];
+    for(i = 1; i < n; i++)
+    {
+        f1 = p[0][i];
+        for(j = 0; j < i; j++)
+        {
+            f1 *= (k - x[j]);
+        }
+        f += f1;
+    }
+    printf("The value of y at x = %lf is %lf.", k, f);
+    return 0;
+}
